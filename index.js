8a18ba35b05ef14486750886f27de954242b525b
Collin Jilbert <cjilbert504@gmail.com>
2020-06-11 10:02:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+class Cat {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        let firstPhrase = `It's me! ${this.name}, the parrot!`;
+        let secondPhrase = `${this.name} says squawk!`;
+        return this.gender === 'male' ? firstPhrase : secondPhrase;
+    }
+}
