4dcc04a202ef8bd12ec1138aae5060119a151b09
Mug <brad.alexander913@gmail.com>
2020-07-05 10:11:47 -0700

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,26 @@ class Cat {
   }
    
   class Dog {
-    //...
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
   }
    
   class Bird {
-    //...
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        if (this.sex === "male"){
+        return `It's me! ${this.name}, the parrot!`
+        }
+        else return `${this.name} says squawk!`
+    }
   }
